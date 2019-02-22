97ea494f9a5b9854de632d284b50bebfbfe8b9e5
ThomasJasonM1 <jason@call-em-all.com>
2019-02-22 16:02:31 -0600

    allows me to search through logs on all machines

--- Test.js
+++ Test.js
@@ -1,78 +1,122 @@
 // External Variables
-const countLinesInFile = require('count-lines-in-file');
-const CsvReadableStream = require('csv-reader');
-const fs = require('fs');
-const path = require('path');
+// const countLinesInFile = require('count-lines-in-file');
+// const CsvReadableStream = require('csv-reader');
+//const fs = require('fs');
+// const path = require('path');
 const { exec } = require('child_process');
 // var mysql = require('mysql');
 // const dotenv = require('dotenv').load();
 
-// Local Variables
-const inputStream = fs.createReadStream('../Test Files/CallLogTestFile.csv', 'utf8');
-const logList = ["Dialing", "NO_USER_RESPONSE", "detectAnswer", "handleLiveAnswer", "playBcAudio", "endCall", "handleTooManyMenuPlays", "handleCause"]
-const targetFilePath = path.resolve(__dirname, '../Test Files/CallLogTestFile.csv');
-const writeStream = fs.createWriteStream('../results.csv');
 
-// Counts the number of rows in my file so I can loop through
-function getData() {
-	return new Promise(function(resolve, reject){
-		countLinesInFile(targetFilePath, (Error, number) => {
-			resolve(number);
 
+
+function runCommand(server) {
+	return new Promise(function(resolve, reject) {
+		let path = 'zgrep -i \'' + '7247756127' + '\' ../../../../Volumes/NASSHARE/SIPLogs/' + server + '*';
+		console.log(path);
+		exec(path, 
+			(error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					resolve(error);
+				}
+			console.log('stderr: ', stderr);
+			console.log('stdout: ', stdout);
+			resolve(stdout);
 		});
 	});
+}	
+
+
+function self (x) {
+	let server = 'SIP';
+	let value1;
+	let value2;
+	if (x < 10) {server = server + 0;}
+	server = server + x + '-20190214';
+	console.log('Server: ', server);
+
+	runCommand(server)
+		.then((value) => {
+			console.log('value: ', value);
+			x = x + 1;
+			if(x < 57) {
+		  		return self(x);
+		  	} else {
+		  		return value2;
+		  	}
+		});
 }
+
+self(1);
+
+
+// Local Variables
+// const inputStream = fs.createReadStream('../Test Files/CallLogTestFile.csv', 'utf8');
+// const logList = ["Dialing", "NO_USER_RESPONSE", "detectAnswer", "handleLiveAnswer", "playBcAudio", "endCall", "handleTooManyMenuPlays", "handleCause"]
+// const targetFilePath = path.resolve(__dirname, '../Test Files/CallLogTestFile.csv');
+// const writeStream = fs.createWriteStream('../results.csv');
+
+// Counts the number of rows in my file so I can loop through
+// function getData() {
+// 	return new Promise(function(resolve, reject){
+// 		countLinesInFile(targetFilePath, (Error, number) => {
+// 			resolve(number);
+
+// 		});
+// 	});
+// }
 
 // Each time this is called, it will exdcute a grep for incoming data
-function runCommand(i) {
-	return new Promise(function(resolve, reject) {
-		var byteCounterStart=  i == 0 ? 0 : (i * 103); // Calculates what is the working row based on bytes
-		var byteCounterEnd = byteCounterStart + 101; // Each row is 101 bytes
-		var inputStream = 
-			fs.createReadStream('../Test Files/CallLogTestFile.csv', {encoding: 'utf8', start: byteCounterStart, end: byteCounterEnd});
+// function runCommand(i) {
+// 	return new Promise(function(resolve, reject) {
+// 		var byteCounterStart=  i == 0 ? 0 : (i * 103); // Calculates what is the working row based on bytes
+// 		var byteCounterEnd = byteCounterStart + 101; // Each row is 101 bytes
+// 		var inputStream = 
+// 			fs.createReadStream('../Test Files/CallLogTestFile.csv', {encoding: 'utf8', start: byteCounterStart, end: byteCounterEnd});
 
-		inputStream
-		    .pipe(CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
-		    .on('data', function (column) {
-		    	exec('zgrep -i \'' + column[0] + '\' ../../../../Volumes/NASSHARE/SIPLogs/' + column[1] + '*', 
-		    		(error, stdout, stderr) => {
-						if (error) {
-							console.error(`exec error: ${error}`);
-							return;
-						}
-						resolve(stdout);
-				});	
-    	})
-	})
-}
+// 		inputStream
+// 		    .pipe(CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
+// 		    .on('data', function (column) {
+// 		    	exec('zgrep -i \'' + column[0] + '\' ../../../../Volumes/NASSHARE/SIPLogs/' + column[1] + '*', 
+// 		    		(error, stdout, stderr) => {
+// 						if (error) {
+// 							console.error(`exec error: ${error}`);
+// 							return;
+// 						}
+// 						resolve(stdout);
+// 				});	
+//     	})
+// 	})
+// }
 
-function printToFile(data, i) {
-	if (!i) {i = 0};
-	if (i <= data) {
-		runCommand(i).then( function (value){
-			i++;
-			var splittingValue = value.split("\n");
-			for (x in splittingValue) {
-				for (y in logList) {
-					if (splittingValue[x].includes(logList[y])) {
-						var result = splittingValue[x].split(" ");
-						writeStream.write(`${result}\n`);
-						writeStream.write("\n");
-					}
-				}
-			}
-			console.log(`Finished row ${i}`);
-			if (i <= data) {
-				printToFile(data, i);
-			}
-		}).catch(function (error){
-			console.log(error);
+// function printToFile(data, i) {
+// 	if (!i) {i = 0};
+// 	if (i <= data) {
+// 		runCommand(i).then( function (value){
+// 			i++;
+// 			var splittingValue = value.split("\n");
+// 			for (x in splittingValue) {
+// 				for (y in logList) {
+// 					if (splittingValue[x].includes(logList[y])) {
+// 						var result = splittingValue[x].split(" ");
+// 						writeStream.write(`${result}\n`);
+// 						writeStream.write("\n");
+// 					}
+// 				}
+// 			}
+// 			console.log(`Finished row ${i}`);
+// 			if (i <= data) {
+// 				printToFile(data, i);
+// 			}
+// 		}).catch(function (error){
+// 			console.log(error);
 
-		})
-	}
-	return;
-}
+// 		})
+// 	}
+// 	return;
+// }
 
-getData()
-	.then(printToFile);
+// getData()
+	// .then(printToFile);
 
